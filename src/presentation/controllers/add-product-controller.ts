e597f68c3c8ba2a549f60958e7c4961cd399297c
mlucasdev <lucasmsantos819@gmail.com>
2023-12-08 09:07:55 -0300

    refactor: add HttpRequest type

--- src/presentation/controllers/add-product-controller.ts
+++ src/presentation/controllers/add-product-controller.ts
@@ -1,12 +1,12 @@
 import { type AddProduct } from 'src/domain/contracts/add-product'
 import { type Controller } from '../contracts'
-import { type HttpResponse } from '../http/http'
+import { type HttpRequest, type HttpResponse } from '../http/http'
 import { badRequest, noContent } from '../http/http-helpers'
 
 export class AddProductController implements Controller {
   constructor (private readonly addProduct: AddProduct) {}
 
-  async handle (httpRequest: any): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const addProductResult = await this.addProduct.perform(httpRequest.body?.name)
     if (addProductResult.isLeft()) {
       return badRequest(addProductResult.value)
